05ed2cb64e7b69287d48671359d7b169fac0b496
Hongsugyeong <olivia258@naver.com>
2022-08-03 11:09:23 +0900

    connection test 완료

--- src/main/java/com/gang/start/test/DBConnectionTest.java
+++ src/main/java/com/gang/start/test/DBConnectionTest.java
@@ -1,10 +1,24 @@
 package com.gang.start.test;
 
+import java.sql.Connection;
+
+import com.gang.start.util.DBConnector;
+
 public class DBConnectionTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		try {
+			Connection con = DBConnector.getConnection();
+			DBConnector.getConnection();
+			System.out.println(con != null);
+			
+		
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
